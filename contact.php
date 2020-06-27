bb1b0c73f04e2ec912510a2bfdb7a27e7f0c23a7
darshshah2802 <darsh.shah@qmetry.com>
2020-06-27 22:03:56 +0530

    no message

---
+++ contact.php
@@ -0,0 +1,508 @@
+<!DOCTYPE html>
+<html>
+  <!-- Mirrored from st.ourhtmldemo.com/new/health-coach/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 13 Jun 2020 08:33:43 GMT -->
+  <head>
+    <meta charset="utf-8" />
+    <title>HB Nutrition</title>
+    <!-- Stylesheets -->
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="css/revolution-slider.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+    />
+    <link href="css/responsive.css" rel="stylesheet" />
+    <!--[if lt IE 9
+      ]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script
+    ><![endif]-->
+    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+  </head>
+
+  <body>
+    <div class="page-wrapper">
+      <!-- Preloader -->
+      <div class="preloader"></div>
+
+      <!-- Main Header-->
+      <header class="main-header">
+        <!-- Main Box -->
+        <div class="main-box">
+          <div class="auto-container">
+            <div class="outer-container clearfix">
+              <!--Logo Box-->
+              <div class="logo-box">
+                <div class="logo">
+                  <a href="index.html"
+                    ><img src="images/required/HUMMUNG.svg" alt=""
+                  /></a>
+                </div>
+              </div>
+
+              <!--Nav Outer-->
+              <div class="nav-outer clearfix">
+                <!-- Main Menu -->
+                <nav class="main-menu">
+                  <div class="navbar-header">
+                    <!-- Toggle Button -->
+                    <button
+                      type="button"
+                      class="navbar-toggle"
+                      data-toggle="collapse"
+                      data-target=".navbar-collapse"
+                    >
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                  </div>
+
+                  <div class="navbar-collapse collapse clearfix">
+                    <ul class="navigation clearfix">
+                      <li><a href="index.html">Home</a></li>
+                      <li>
+                        <a href="./products.html">Products</a>
+                      </li>
+                      <li>
+                        <a href="./blogs.html">Blog</a>
+                      </li>
+                      <li>
+                        <a href="./about-us.html">About Us</a>
+                      </li>
+                      <li class="current">
+                        <a href="./contact.html">Contact Us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+                <!-- Main Menu End-->
+              </div>
+              <!--Nav Outer End-->
+
+              <!-- Hidden Nav Toggler -->
+              <div class="nav-toggler">
+                <button class="hidden-bar-opener">
+                  <span class="icon fa fa-bars"></span>
+                </button>
+              </div>
+              <!-- / Hidden Nav Toggler -->
+
+              <!--Cart Btn-->
+            </div>
+          </div>
+        </div>
+      </header>
+      <!--End Main Header -->
+
+      <!-- Hidden Navigation Bar -->
+      <section class="hidden-bar right-align">
+        <div class="hidden-bar-closer">
+          <button class="btn"><i class="fa fa-close"></i></button>
+        </div>
+
+        <!-- Hidden Bar Wrapper -->
+        <div class="hidden-bar-wrapper">
+          <!-- .logo -->
+          <div class="logo text-center">
+            <a href="index.html"
+              ><img src="images/required/HUMMUNG.svg" alt=""
+            /></a>
+          </div>
+          <!-- /.logo -->
+
+          <!-- .Side-menu -->
+          <div class="side-menu">
+            <!-- .navigation -->
+            <ul class="navigation">
+              <li><a href="./index.html">Home</a></li>
+              <li>
+                <a href="./products.html">Products</a>
+              </li>
+              <li>
+                <a href="./blogs.html">Blogs</a>
+              </li>
+              <li>
+                <a href="./about-us.html">About Us</a>
+              </li>
+              <li class="current">
+                <a href="./contact.html">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <!-- /.Side-menu -->
+
+          <div class="social-icons">
+            <ul>
+              <li>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- / Hidden Bar Wrapper -->
+      </section>
+      <!-- / Hidden Bar -->
+
+      <!--Page Title-->
+      <section
+        class="page-title"
+        style="background-image: url(images/background/bg-page-title-1.jpg);"
+      >
+        <div class="auto-container">
+          <h1>Contact</h1>
+          <h3 class="styled-font">We Offer healthier lifestyle.</h3>
+        </div>
+      </section>
+
+      <!--Page Info-->
+      <section class="page-info">
+        <div class="auto-container clearfix">
+          <div class="breadcrumb-outer">
+            <ul class="bread-crumb clearfix">
+              <li><a href="index.html">Home</a></li>
+              <li class="active">Contact Us</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <!--Contact Section-->
+      <section class="contact-section">
+        <div class="auto-container">
+          <div class="row clearfix">
+            <!--Form Column -->
+            <div
+              class="column form-column pull-right col-lg-6 col-md-12 col-sm-12 col-xs-12"
+            >
+              <div class="sec-title">
+                <h2>Send Us Message</h2>
+                <div class="separator"></div>
+              </div>
+              <!--form-box-->
+              <div class="form-box default-form">
+                <div class="contact-form default-form">
+                  <form
+                    method="post"
+                    action="mail.php"
+                    id="contact-form"
+                  >
+                    <div class="row clearfix">
+                      <div class="form-group col-md-6 col-sm-6 col-xs-12">
+                        <input
+                          type="text"
+                          name="username"
+                          value=""
+                          placeholder="Your Name *"
+                          required
+                        />
+                      </div>
+
+                      <div class="form-group col-md-6 col-sm-6 col-xs-12">
+                        <input
+                          type="email"
+                          name="email"
+                          value=""
+                          placeholder="Your Mail *"
+                          required
+                        />
+                      </div>
+
+                      <div class="form-group col-md-6 col-sm-6 col-xs-12">
+                        <input
+                          type="text"
+                          name="phone"
+                          value=""
+                          placeholder="Phone Number"
+                        />
+                      </div>
+
+                      <div class="form-group col-md-6 col-sm-6 col-xs-12">
+                        <select name="subject">
+                          <option>Subject</option>
+                          <option>Wooden Flooring</option>
+                          <option>Metal Flooring</option>
+                          <option>Concrete Flooring</option>
+                        </select>
+                      </div>
+
+                      <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                        <textarea
+                          name="message"
+                          placeholder="Message *"
+                        ></textarea>
+                      </div>
+                      <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                        <button type="submit" class="theme-btn btn-style-one">
+                          Submit now
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            <!--Column-->
+            <div
+              class="column info-column pull-left col-lg-6 col-md-8 col-sm-12 col-xs-12"
+            >
+              <div class="sec-title">
+                <h2>Get In Touch</h2>
+                <div class="separator"></div>
+              </div>
+              <!--Info Tabs-->
+              <div class="tabs-box info-tabs">
+                <!--Tabs Content-->
+                <div class="tabs-content">
+                  <!--Tab / Active Tab-->
+                  <div class="tab active-tab" id="info-tab-1">
+                    <div class="desc-text">
+                      How all this mistaken idea of denouncing pleasure and
+                      praising pain was born and we will you complete system.
+                    </div>
+                    <h3 class="location-title">Newyork City :</h3>
+                    <div class="info-style-one">
+                      <ul>
+                        <li>
+                          <div class="icon-box">
+                            <span class="fa fa-globe"></span>
+                          </div>
+                          <h4>Address :</h4>
+                          <div class="text">
+                            21/17, Level 4 Steel Rock St, Newyork City
+                            <br />Victoria 3000, USA.
+                          </div>
+                        </li>
+                        <li>
+                          <div class="icon-box">
+                            <span class="flaticon-envelope"></span>
+                          </div>
+                          <h4>Ask Some Thing Us :</h4>
+                          <div class="text">Mail@HealthCoach.com</div>
+                        </li>
+                        <li>
+                          <div class="icon-box">
+                            <span class="flaticon-technology-1"></span>
+                          </div>
+                          <h4>Call Us:</h4>
+                          <div class="text">+123-456-7890 , 1-800-425-8910</div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--End Info Tabs-->
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--Map Section-->
+      <section class="map-section">
+        <div class="map-outer">
+          <!--Map Canvas-->
+          <div
+            class="map-canvas"
+            data-zoom="10"
+            data-lat="23.815811"
+            data-lng="90.412580"
+            data-type="roadmap"
+            data-hue="#fc721e"
+            data-title="Dhaka"
+            data-content="Dhaka 1000-1200, Bangladesh<br><a href='mailto:info@youremail.com'>info@youremail.com</a>"
+            style="height: 480px;"
+          ></div>
+        </div>
+      </section>
+
+      <!--Main Footer-->
+      <footer class="main-footer">
+        <div class="auto-container">
+          <!--Widgets Section-->
+          <div class="widgets-section">
+            <div class="row clearfix">
+              <!--Big Column-->
+              <div class="big-column col-md-6 col-sm-12 col-xs-12">
+                <div class="row clearfix">
+                  <!--Footer Column-->
+                  <div class="footer-column col-md-6 col-sm-6 col-xs-12">
+                    <div class="footer-widget about-widget">
+                      <div class="footer-logo">
+                        <figure>
+                          <a href="index.html"
+                            ><img src="images/required/HUMMUNG.svg" alt=""
+                          /></a>
+                        </figure>
+                      </div>
+                      <div class="widget-content">
+                        <div class="text">
+                          We Hummingbird nutrition by Potencia wellness are a
+                          prominent manufacturer, supplier, exporter of a best
+                          quality assortment of wellness products, due to the
+                          outbreak of COVID19, we have launched our covid care
+                          range of products to help and serve the humanity in
+                          this pandemic. The products are highly appreciated in
+                          the market and are unmatched quality and optimum
+                          performances and we provide products with leading
+                          market price within promised time.
+                        </div>
+                        <a href="#" class="theme-btn btn-style-four"
+                          >Purchase Now</a
+                        >
+                      </div>
+                    </div>
+                  </div>
+
+                  <!--Footer Column-->
+                  <div class="footer-column col-md-6 col-sm-6 col-xs-12">
+                    <div class="footer-widget links-widget">
+                      <h2>Contact Us</h2>
+                      <div class="widget-content">
+                        <ul class="contact-info">
+                          <li>
+                            <span class="icon fa fa-phone"></span
+                            ><strong>Phone:</strong> +91 9316041617, <br />
+                            079-40028029
+                          </li>
+                          <li>
+                            <span class="icon fa fa-mail-forward"></span
+                            ><strong>Email:</strong>
+                            <a href="mailto:info@hbnutricians.com"
+                              >info@hbnutricians.com</a
+                            >
+                          </li>
+                          <li>
+                            <span class="icon fa fa-location-arrow"></span
+                            ><strong>Address:</strong>
+                            <a href="https://goo.gl/maps/gmA8dp7upcCTCcWs7">
+                              A-1111 ,Titanium Height Opposite Vodafone
+                              House,Corporate Road,Ahmedabad-380015</a
+                            >
+                          </li>
+                          <li>
+                            <span class="icon fa fa-whatsapp"></span
+                            ><strong>Text Us</strong>
+                            <a
+                              href="https://api.whatsapp.com/send?phone=919316041617"
+                              >+91 9316041617</a
+                            >
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+
+                    <div class="footer-widget newsletter-widget">
+                      <div class="footer-widget tags-widget">
+                        <h2>Popular Tags</h2>
+                        <div class="widget-content">
+                          <ul class="tags-list">
+                            <li><a href="#">Yoga</a></li>
+                            <li><a href="#">Fitness</a></li>
+                            <li><a href="#">Workouts</a></li>
+                            <li><a href="#">Foods</a></li>
+                            <li><a href="#">Nutrition</a></li>
+                            <li><a href="#">Diet</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!--Big Column-->
+              <div class="big-column col-md-6 col-sm-12 col-xs-12">
+                <div class="row clearfix">
+                  <!--Footer Column-->
+                  <div class="footer-column col-md-6 col-sm-6 col-xs-12">
+                    <div class="footer-widget posts-widget">
+                      <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58768.767594953344!2d71.42955776844627!3d22.985263270678796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39596230e7daa7e9%3A0xed74cf7018dc63!2sDhrangadhra%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1592046942894!5m2!1sen!2sin"
+                        width="600"
+                        height="450"
+                        frameborder="0"
+                        style="border: 0;"
+                        allowfullscreen=""
+                        aria-hidden="false"
+                        tabindex="0"
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!--Footer Bottom-->
+        <div class="footer-bottom">
+          <div class="auto-container">
+            <div class="clearfix">
+              <div class="pull-left">
+                <div class="copyright">
+                  &copy; Copyright 2020. All Rights Reserved.
+                </div>
+              </div>
+              <div class="pull-right">
+                <ul class="footer-social">
+                  <li>
+                    <a href="#"><span class="fa fa-facebook-f"></span></a>
+                  </li>
+                  <li>
+                    <a href="#"><span class="fa fa-twitter"></span></a>
+                  </li>
+                  <li>
+                    <a href="#"><span class="fa fa-google-plus"></span></a>
+                  </li>
+                  <li>
+                    <a href="#"><span class="fa fa-linkedin"></span></a>
+                  </li>
+                  <li>
+                    <a href="#"><span class="fa fa-instagram"></span></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!--End pagewrapper-->
+
+    <!--Scroll to top-->
+    <div class="scroll-to-top scroll-to-target" data-target=".main-header">
+      <span class="fa fa-angle-up"></span>
+    </div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/jquery.fancybox-media.js"></script>
+    <script src="js/owl.js"></script>
+    <script src="js/validate.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/script.js"></script>
+
+    <!--Google Map APi Key-->
+    <script src="http://maps.google.com/maps/api/js?key="></script>
+    <script src="js/map-script.js"></script>
+    <!--End Google Map APi-->
+  </body>
+
+  <!-- Mirrored from st.ourhtmldemo.com/new/health-coach/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 13 Jun 2020 08:36:47 GMT -->
+</html>
